4375ce14df76f640dd0e6b3acb29a5a8ef062b5a
fengjinlong <18618166564@163.com>
2019-06-04 14:23:46 +0800

    a

--- app.js
+++ app.js
@@ -1,56 +1,15 @@
-const Vue = require('vue')
-const server = require('express')()
-// const renderer = require('vue-server-renderer').createRenderer()
+import Vue from 'vue'
+import App from './App.vue'
+import { createRouter } from './router'
 
-const createApp = function (context) {
-  return new Vue({
-    data: {
-      url: context.url
-    },
-    template: `<div>访问的 URL 是： {{ url }}</div>`
+// 导出一个工厂函数，用于创建新的
+// 应用程序、router 和 store 实例
+export function createApp () {
+  const router = createRouter()
+  const app = new Vue({
+    router,
+    // 根实例简单的渲染应用程序组件。
+    render: h => h(App)
   })
+  return {app, router}
 }
-
-server.get('*', (req, res) => {
-  // const app = new Vue({
-  //   data: {
-  //     url: req.url
-  //   },
-  //   template: `<div>访问的 URL 是： {{ url }}</div>`
-  // })
-
-  const context1 = { url: req.url }
-  const app = createApp(context1)
-
-  // renderer.renderToString(app, (err, html) => {
-  //   if (err) {
-  //     res.status(500).end('Internal Server Error')
-  //     return
-  //   }
-  //   res.end(`
-  //     <!DOCTYPE html>
-  //     <html lang="en">
-  //       <meta charset="UTF-8">
-  //       <head><title>Hello</title></head>
-  //       <body>${html}</body>
-  //     </html>
-  //   `)
-  // })
-
-  const renderer = require('vue-server-renderer').createRenderer({
-    template: require('fs').readFileSync('./staticHtml/index.template.html', 'utf-8')
-  })
-  const context = {
-    title: 'hello1',
-    meta: `
-    <meta charset="UTF-8">
-    `
-  }
-  
-  renderer.renderToString(app, context, (err, html) => {
-    console.log(html) // html 将是注入应用程序内容的完整页面
-    res.end(html)
-  })
-})
-
-server.listen(8080)
